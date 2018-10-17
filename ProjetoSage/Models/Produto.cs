36b998f52c7f78b816c1a0ef1bfeb3e0219b6e69
hiagor <hiagob.reis@hotmail.com>
2018-10-17 00:46:08 -0300

    resto do commit

---
+++ ProjetoSage/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoSage.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
---
+++ ProjetoSage/ORM/ProdutoMap.cs
@@ -0,0 +1,24 @@
+﻿using ProjetoSage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjetoSage.ORM
+{
+    public class ProdutoMap : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoMap()
+        {
+            ToTable("PRODUTOS");
+            HasKey(a => a.Id);
+            Property(a => a.Id).HasColumnName("ID_PROD").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(a => a.Nome).HasColumnName("NOM_PROD").IsRequired();
+            Property(a => a.Preco).HasColumnName("PREC_PROD").IsRequired();
+            Property(a => a.Categoria).HasColumnName("CAT_PROD").IsRequired();
+            Property(a => a.Descricao).HasColumnName("DESC_PROD").IsRequired();
+        }
+    }
+}
